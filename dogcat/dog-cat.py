c7dfcd60ca91b2f4028eaa5fcabbef91d366fb52
Truong Trung Nghia <nghiatt4@fsoft.com.vn>
2017-12-05 10:41:22 +0900

    Load data in batch OK

--- dogcat/dog-cat.py
+++ dogcat/dog-cat.py
@@ -10,6 +10,7 @@ from preprocess import create_tfrecords
 
 DEFAULT_CONFIG_VALUES = {
     'learning_rate': 1e-3,
+    'batch_size': 128,
 }
 # Default number of classes in pre-trained models (ImageNet)
 MODEL_DEFAULT_CLASS_COUNT = 1024
@@ -35,7 +36,9 @@ def get_tfrecord_files(data_dir: str, split_name: str) -> List[str]:
     return glob.glob(pattern)
 
 
-def _parse_example_proto(proto: tf.train.Example):
+def _parse_example_proto(proto: tf.train.Example,
+                         image_size: int = 299, channels: int = 3)\
+        -> Tuple[tf.Tensor, tf.Tensor]:
     """
 
     :param proto:
@@ -47,23 +50,26 @@ def _parse_example_proto(proto: tf.train.Example):
     }
     features = tf.parse_example(proto, features=feature_map)
     image = tf.decode_raw(features['image'], tf.float32)
-    image = tf.reshape(image, [15, 299, 299, 3])
+    image = tf.reshape(image, [-1, image_size, image_size, channels])
     label = tf.cast(features['label'], tf.float32)
     return image, label
 
 
-def get_tfrecord_loader(file_names: tf.placeholder) -> tf.contrib.data.Iterator:
+def get_tfrecord_loader(file_names: tf.placeholder, batch_size: int = None, buffer_size: int = 1000)\
+        -> tf.contrib.data.Iterator:
     """Create a dataset to read tfrecord files and return its iterator.
 
     The iterator expects a list of tfrecord file names to be fed to
     its 'file_names' placeholder.
 
     :param file_names: tf.placeholder
+    :param batch_size: Number of images in each batch
+    :param buffer_size: See tf.contrib.Dataset.shuffle
     :return: Tensor of type Iterator
     """
     dataset = TFRecordDataset(file_names)
-    dataset = dataset.shuffle(buffer_size=1000)
-    dataset = dataset.batch(128)
+    dataset = dataset.shuffle(buffer_size=buffer_size)
+    dataset = dataset.batch(batch_size)
     dataset = dataset.map(_parse_example_proto)
     return dataset.make_initializable_iterator()
 
@@ -100,7 +106,7 @@ def train(args: argparse.Namespace) -> None:
     """
     input_dir = create_tfrecords(args.data_dir)
     file_names = tf.placeholder(tf.string, shape=[None])
-    iterator = get_tfrecord_loader(file_names)
+    iterator = get_tfrecord_loader(file_names, args.batch_size)
     next_batch = iterator.get_next()
 
     train_file_names = get_tfrecord_files(input_dir, 'train')
@@ -139,7 +145,8 @@ if __name__ == '__main__':
                         help='Momentum')
     parser.add_argument('--num-epochs', '-e', type=int, default=1,
                         help='Number of training epochs. Ignored if --train is not specified')
-    parser.add_argument('--batch-size', '-b', type=int, default=128,
+    parser.add_argument('--batch-size', '-b', type=int,
+                        default=DEFAULT_CONFIG_VALUES['batch_size'],
                         help='Number of images to load in every batch')
     parser.add_argument('--train', action='store_true',
                         help='Activate training mode')
--- dogcat/preprocess.py
+++ dogcat/preprocess.py
@@ -139,9 +139,8 @@ def convert_to_tfrecords(output_dir: str,
             for i in range(start_idx, end_idx):
                 sys.stdout.write('\rConverting image %d/%d shard %d' % (i + 1, total, shard_id + 1))
                 sys.stdout.flush()
-
-            example_proto = _create_example_proto(paths[i], labels[i])
-            writer.write(example_proto.SerializeToString())
+                example_proto = _create_example_proto(paths[i], labels[i])
+                writer.write(example_proto.SerializeToString())
 
         sys.stdout.write('\n')
         sys.stdout.flush()
