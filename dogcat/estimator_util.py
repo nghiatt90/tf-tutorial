463704e4ba847ea3a30016ae00540b32e428ec5d
Truong Trung Nghia <nghiatt4@fsoft.com.vn>
2017-12-06 10:25:32 +0900

    Parameterize load_image function

--- dogcat/estimator_util.py
+++ dogcat/estimator_util.py
@@ -12,15 +12,16 @@ def bytes_feature(value: ByteString) -> tf.train.Feature:
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 
-def load_image(path: str) -> np.ndarray:
+def load_image(path: str, image_size: int = 299) -> np.ndarray:
     """
-    Read an image, resize to 224x224.
+    Read an image and resize it for training/testing.
 
     :param path: Path to image
+    :param image_size: Desirable size for training or testing
     :return: Image after resizing
     """
     image = cv2.imread(path)
-    image = cv2.resize(image, (299, 299), interpolation=cv2.INTER_CUBIC)
+    image = cv2.resize(image, (image_size, image_size), interpolation=cv2.INTER_CUBIC)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB
     image = image.astype(np.float32)
     return image
